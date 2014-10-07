f0699f8afcc1ca96f97acba21b2dce7f268252fb
Ian McDevitt <ian.mcdevitt.4@gmail.com>
2014-10-07 11:54:18 -0400

    BIG BANG.
    The tool now does things. It's pretty, it has creatures (not in this commit), it's sortable.

--- js/controllers/ShapeshiftCtrl.js
+++ js/controllers/ShapeshiftCtrl.js
@@ -6,19 +6,91 @@ angular.module('dnd5e.controllers.shapeshift', []).controller('shapeshiftCtrl', 
 	$scope.beasts = beasts;
 	$scope.min_cr = 0;
 	$scope.max_cr = 30;
-	$scope.swim = 'false';
-	$scope.flight = 'false';
-	$scope.size = 1;
+	$scope.swim = '2';
+	$scope.flight = '2';
+	$scope.elemental = '2';
+	$scope.size = ["0.25","0.5","1","2","3"];
+	$scope.enemy = {};
+	$scope.enemy.ac = 14;
+	$scope.enemy.attack = 6;
+	$scope.enemy.dph = 10;
+
+	$scope.sort = 'name';
+	$scope.sort_dir = false;
+
+	$scope.recalculate_all_dpr = function() {
+		$scope.beasts.forEach(function(beast) {
+			beast.dpr = $scope.dpr_calc(beast);
+			beast.survivability = $scope.survivability_calc(beast);
+		});
+	};
+
+	$scope.dpr_calc = function(beast) {
+		return Math.min(((beast.attack + 20 - $scope.enemy.ac) / 20), 0.95) * beast.damage;
+	};
+	$scope.survivability_calc = function(beast) {
+		return beast.hp / (Math.min(((20 + $scope.enemy.attack - beast.ac) / 20), 0.95) * $scope.enemy.dph);
+	};
+
+	$scope.beasts.forEach(function(beast) {
+		beast.dpr = $scope.dpr_calc(beast);
+		beast.survivability = $scope.survivability_calc(beast);
+	});
+
+	$scope.$watch('enemy.ac', function() {
+		$scope.recalculate_all_dpr();
+	});
+	$scope.$watch('enemy.attack', function() {
+		$scope.recalculate_all_dpr();
+	});
+	$scope.$watch('enemy.dph', function() {
+		$scope.recalculate_all_dpr();
+	});
+
+	$scope.titles = [
+		{title: 'Name', field: 'name'},
+		{title: 'Size', field: 'size'},
+		{title: 'CR', field: 'cr'},
+		{title: 'AC', field: 'ac'},
+		{title: 'HP', field: 'hp'},
+		{title: 'Survivability', field: 'survivability', tooltip: "'Number of rounds this creature will survive against an enemy with a +6 to attacks and an average of 10 damage.'"},
+		{title: 'Speed', field: 'speed'},
+		{title: 'Swim', field: 'swim'},
+		{title: 'Flight', field: 'flight'},
+		{title: 'Attacks', field: 'attacks', tooltip: "'Number of attacks this creature can make per round.'"},
+		{title: 'Modifier', field: 'attack'},
+		{title: 'DPH', field: 'damage', tooltip: "'Average damage creature does on a hit.'"},
+		{title: 'DPR', field: 'dpr', tooltip: "'Average damage per round against a target with 14 AC.'"}
+	];
+
+	$scope.sizes = {
+		'0.25': 'Tiny',
+		'0.5': 'Small',
+		'1': 'Medium',
+		'2': 'Large',
+		'3': 'Huge'
+	};
 
 	$scope.filterBeasts = function(beast) {
-		console.log(beast, $scope);
 		if(
-			   beast.cr < $scope.min_cr
-			|| beast.cr > $scope.max_cr
-			|| beast.swim == true && $scope.swim == 'false'
-			|| beast.flight == true && $scope.flight == 'false'
-			|| beast.size != +$scope.size
-		) { console.log(beast.cr, $scope.min_cr, $scope.max_cr, beast.swim, $scope.swim, beast.flight, $scope.flight, beast.size, $scope.size); return false; }
+			beast.cr < $scope.min_cr ||
+			beast.cr > $scope.max_cr ||
+			beast.swim > 0 && $scope.swim == '0' ||
+			beast.swim === 0 && $scope.swim == '1' ||
+			beast.flight > 0 && $scope.flight == '0' ||
+			beast.flight === 0 && $scope.flight == '1' ||
+			$scope.size.indexOf(''+beast.size) === -1 ||
+			beast.name.indexOf('Elemental') !== -1 && $scope.elemental === false
+		) { return false; }
 		return true;
-	}	
+	};
+
+	$scope.evaluateBeast = function(beast) {
+		return beast[$scope.sort];
+	};
+
+	$scope.re_sort = function(field) {
+		$scope.sort_dir = ( $scope.sort == field ? !$scope.sort_dir : true );
+		$scope.sort = field;
+	};
 }]);
--- js/directives.js
+++ js/directives.js
@@ -2,9 +2,35 @@
 
 /* Directives */
 
-angular.module('dnd5e.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+angular.module('dnd5e.directives', [])
+    .directive('qtip', function() {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                var my = attrs.my || 'bottom center'
+                  , at = attrs.at || 'top center'
+                  , qtipClass = attrs.class || 'qtip'
+                  , content;
+
+                if (attrs.title) {
+                    content = {'title': attrs.title, 'text': attrs.content}
+                } else {
+                    content = attrs.content
+                }
+
+                $(element).qtip({
+                    content: content,
+                    position: {
+                        my: my,
+                        at: at,
+                        target: element
+                    },
+                    hide: {
+                        fixed : true,
+                        delay : 100
+                    },
+                    style: qtipClass
+                });
+            }
+        }
+    });
